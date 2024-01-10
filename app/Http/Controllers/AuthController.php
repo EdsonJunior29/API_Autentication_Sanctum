e85d1025b01ad38870832dd1781b1f08ef7f102f
Edson Junior <edson.junior@mereo.com>
2024-01-10 12:47:15 -0300

    cria o métoddo para logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -52,6 +52,10 @@ class AuthController extends Controller
 
     public function logout()
     {
-        return response()->json('this is my logout method.');
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => 'You have successfuly been logged out and your token has been deleted.'
+        ]);
     }
 }

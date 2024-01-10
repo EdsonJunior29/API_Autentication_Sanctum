b52431ea2f7f0bc42645749a465be77a16a9cb26
Edson Junior <edson.junior@mereo.com>
2024-01-09 21:52:11 -0300

    Criando as rotas para login, registro e logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login()
+    {
+        return 'this is my login';
+    }
+
+    public function register()
+    {
+        return response()->json('this is my register method.');
+    }
+
+    public function logout()
+    {
+        return response()->json('this is my logout method.');
+    }
+}

9d7764d6d23d0ae06e1fe7341e8d0e72c971fb33
Edson Junior <edson.junior@mereo.com>
2024-01-09 21:35:38 -0300

    Criando o trait para retorno das requisições http.

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -3,5 +3,22 @@
 namespace App\Traits;
 
 trait HttpResponses {
-    
+
+    protected function success($data, $message = null, $code = 200)
+    {
+        return response()->json([
+            'status' => 'Request was succesful.',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function error($data, $message = null, $code)
+    {
+        return response()->json([
+            'status' => 'Error has occurred.',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
 }
